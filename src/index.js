a869c9dabda3e32b1cfb9a6169357eafd3ef4daa
Thomas Bernal <thhomasgt99@gmail.com>
2022-04-25 20:48:50 -0500

    Se arreglaron las etiquetas root duplicadas

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './routes/App';
+import '@styles/vars.scss';
+
 
 ReactDOM.render(<App />, document.getElementById('app'));
